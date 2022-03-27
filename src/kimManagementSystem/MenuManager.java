32c491951aed1a4796d90a08c2aee6707b06a7f3
kamde <kamde@LAPTOP-RFUNGDN3>
2022-03-27 19:54:03 +0900

    edit contents

--- src/kimManagementSystem/MenuManager.java
+++ src/kimManagementSystem/MenuManager.java
@@ -5,68 +5,48 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-	    	Scanner input = new Scanner(System.in);
-	  	    int num = 5;
-		
-		    while (num != 6) {
-			        System.out.println("*** Student Management System menu ***");
-		         	System.out.println("1. Add Students");
-		        	System.out.println("2. Delete Students");
-			        System.out.println("3. Edit Students");
-		        	System.out.println("4. View Students");
-		         	System.out.println("5. Show a menu");
-		        	System.out.println("6. Exit");
-		        	System.out.println("Select one number between 1-6:");
-		        	num = input.nextInt();
-		        	if(num == 1 ) {
-			        	addStudent();
-		        	}
-		        	else if (num == 2) {
-		        		deleteStudent();
-		        	}
-		        	else if (num == 3) {
-			        	editStudent();
-		        	}
-		        	else if (num == 4) {
-		        		viewStudent();
-		            }
-		        	else {
-		        		continue;
-		     	}
-
-        	}
-
-   }
+		Scanner input = new Scanner(System.in);
+  	    int num = 4;
 	
-	public static void addStudent() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Student ID:");
-		int studentId = input.nextInt();
-		System.out.print("Student Name:");
-		String studentName = input.next();
-		System.out.print("Email Address:");
-		String studentEmail = input.next();
-		System.out.println(studentEmail);
-		System.out.print("Phone number:");
-		String studentPhone = input.next();
-	}
-	public static void deleteStudent() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Student ID:");
-		int studentId = input.nextInt();
+	    while (num != 5) {
+		        System.out.println("*** Accouunt Management System menu ***");
+	         	System.out.println("1. Add Money");
+	        	System.out.println("2. Delete Money");
+	        	System.out.println("3. View Money");
+	         	System.out.println("4. Show a menu");
+	        	System.out.println("5. Exit");
+	        	System.out.print("Select one number between 1-6:");
+	        	num = input.nextInt();
+	        	if(num == 1 ) {
+		        	addMoney();
+	        	}
+	        	else if (num == 2) {
+	        		deleteMoney();
+	        	}
+	        	else if (num == 3) {
+	        		viewMoney();
+	            }
+	        	else {
+	        		continue;
+	     	}
 
-	}
-	public static void editStudent() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Student ID:");
-		int studentId = input.nextInt();
+    	}
 
-	}
-	public static void viewStudent() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Student ID:");
-		int studentId = input.nextInt();
+}
 
+    public static void addMoney() {
+    	Scanner input = new Scanner(System.in);
+	    System.out.print("how much?:");
+    	int studentId = input.nextInt();
+    }
+    public static void deleteMoney() {
+    	Scanner input = new Scanner(System.in);
+    	System.out.print("how much:");
+	    int studentId = input.nextInt();
+    }
+    public static void viewMoney() {
+    	Scanner input = new Scanner(System.in);
+    	System.out.print("now money:");
+    	int studentId = input.nextInt();
 	}
-	
 }
