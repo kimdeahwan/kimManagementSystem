7b6135f2aa1da31b0cdb04c1077d728e071005a6
kamde <kamde@LAPTOP-RFUNGDN3>
2022-04-03 17:22:13 +0900

    added class.java

--- src/MyaccountManagementSystem/MenuManager.java
+++ src/MyaccountManagementSystem/MenuManager.java
@@ -3,9 +3,9 @@ package MyaccountManagementSystem;
 import java.util.Scanner;
 
 public class MenuManager {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		AccountManger accountManger = new AccountManger(input);
   	    int num = 4;
 	
 	    while (num != 5) {
@@ -18,35 +18,18 @@ public class MenuManager {
 	        	System.out.print("Select one number between 1-6:");
 	        	num = input.nextInt();
 	        	if(num == 1 ) {
-		        	addMoney();
+	        		accountManger.addMoney();
 	        	}
 	        	else if (num == 2) {
-	        		deleteMoney();
+	        		accountManger.deleteMoney();
 	        	}
 	        	else if (num == 3) {
-	        		viewMoney();
+	        		accountManger.viewMoney();
 	            }
 	        	else {
 	        		continue;
-	     	}
+	     	 }
 
     	}
-
-}
-
-    public static void addMoney() {
-    	Scanner input = new Scanner(System.in);
-	    System.out.print("how much?:");
-    	int studentId = input.nextInt();
-    }
-    public static void deleteMoney() {
-    	Scanner input = new Scanner(System.in);
-    	System.out.print("how much:");
-	    int studentId = input.nextInt();
-    }
-    public static void viewMoney() {
-    	Scanner input = new Scanner(System.in);
-    	System.out.print("now money:");
-    	int studentId = input.nextInt();
 	}
 }
