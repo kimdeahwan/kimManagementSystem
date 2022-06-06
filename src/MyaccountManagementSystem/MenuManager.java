b69cae373b021fa193cb8bec9906dfc235e8e66a
kamde <kamde@LAPTOP-RFUNGDN3>
2022-06-06 23:01:11 +0900

    added gui.java

--- src/MyaccountManagementSystem/MenuManager.java
+++ src/MyaccountManagementSystem/MenuManager.java
@@ -1,42 +1,9 @@
 package MyaccountManagementSystem;
 
-import java.util.Scanner;
-
-public class MenuManager {
+class  ScoreMain
+{
 	public static void main(String[] args) 
-			throws java.util.InputMismatchException {
-		Scanner input = new Scanner(System.in);
-		AccountManger accountManger = new AccountManger(input );
-  	    int num = 4;
-  	    
-	    while (num != 5) {
-		        System.out.println("*** Accouunt Management System menu ***");
-	         	System.out.println("1. Add Money");
-	        	System.out.println("2. Delete Money");
-	        	System.out.println("3. View Money");
-	         	System.out.println("4. Show a menu");
-	        	System.out.print("Select one number between 1-4:");
-	        	try {
-	        		num = input.nextInt();
-	      		}
-	      		catch(java.util.InputMismatchException e) {
-	      			System.out.println("ERORR");
-	      			throw e;
-	      		}
-	        	num = input.nextInt();
-	        	if(num == 1 ) {
-	        		accountManger.addMoney();
-	        	}
-	        	else if (num == 2) {
-	        		accountManger.deleteMoney();
-	        	}
-	        	else if (num == 3) {
-	        		accountManger.viewMoney();
-	            }
-	        	else {
-	        		continue;
-	     	 }
-
-    	}
+	{
+		new AccountManger();
 	}
 }
